297725f620d088ff8e34d48df7e55e600afd2f4c
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-02-27 18:45:21 +0200

    Add validation to check if book.inventory != 0

--- borrowing/models.py
+++ borrowing/models.py
@@ -9,3 +9,25 @@ class Borrowing(models.Model):
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="borrowings")
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="borrowings")
+
+    @staticmethod
+    def validate_book_inventory(book: "Book") -> None:
+        if book.inventory <= 0:
+            raise ValidationError(
+                {
+                    "book": f"The book {book.title} is out of stock!"
+                }
+            )
+
+    def clean(self):
+        Borrowing.validate_book_inventory(self.book)
+
+    def save(
+        self,
+        force_insert = ...,
+        force_update = ...,
+        using = ...,
+        update_fields = ...,
+    ):
+        self.full_clean()
+        self.save(force_insert, force_update, using, update_fields)
