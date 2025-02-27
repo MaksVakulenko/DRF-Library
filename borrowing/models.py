17fcd2e38faff0bb4df5c04f10f10364f302a0bc
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-02-27 17:02:37 +0200

    Create basic borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    """A model for borrowing"""
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="borrowings")
