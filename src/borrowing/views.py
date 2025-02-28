17fd09e8ee3bb544ee1322321f04847c37673c91
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-02-28 16:03:11 +0200

    Create view and urls for Borrowing model

--- src/borrowing/views.py
+++ src/borrowing/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        return BorrowingSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
