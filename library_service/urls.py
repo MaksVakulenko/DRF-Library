ec17587cad81a0214f62ce9e48934a133a295496
Sichkaridze <dima.sichkar2003@gmail.com>
2025-02-28 13:55:42 +0200

    Add migrations and correct mistakes

--- library_service/urls.py
+++ library_service/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/book/", include("book.urls", namespace="book")),
-    path("api/payment/", include("payment.urls", namespace="payment")),
-    path("api/user/", include("user.urls", namespace="user")),
+
+    # Spectacular:
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    path("api/", include("book.urls", namespace="book")),
+    path("api/", include("user.urls", namespace="user")),
+    path("api/", include("payment.urls", namespace="payment")),
 ] + debug_toolbar_urls()
--- user/serializers.py
+++ user/serializers.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-    role = serializers.SerializerMethodField()
-
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "first_name", "last_name", "password")
