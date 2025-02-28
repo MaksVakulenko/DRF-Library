3717078f035e6fb5af3ca19dd02cd19d7a5119a0
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2025-02-28 15:54:00 +0100

    rename api/book/<resource>/ to api/library/<resource>/

--- library_service/urls.py
+++ library_service/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-    path("api/book/", include("book.urls", namespace="book")),
+    path("api/library/", include("book.urls", namespace="book")),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/payment/", include("payment.urls", namespace="payment")),
 ] + debug_toolbar_urls()
