8b0b7078dda698eeb909de2322fd78c152e66d44
Mariana Lishchynska <lishchynska.m@gmail.com>
2025-03-02 18:35:28 +0200

    add serializers and views for payment

--- src/payment/urls.py
+++ src/payment/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 
-from payment.views import StripeSuccessAPI, StripeCancelAPI
-
+from payment.views import StripeSuccessAPI, StripeCancelAPI, PaymentListView, PaymentDetailView
 
 app_name = "payment"
 
 urlpatterns = [
     path("stripe/success/", StripeSuccessAPI.as_view(), name="stripe-success"),
     path("stripe/cancel/", StripeCancelAPI.as_view(), name="stripe-cancel"),
+    path("payments/", PaymentListView.as_view(), name="payment-list"),
+    path("payments/<int:pk>/", PaymentDetailView.as_view(), name="payment-detail"),
 ]
--- src/payment/views.py
+++ src/payment/views.py
@@ -3,13 +3,13 @@ import datetime
 import stripe
 from django.conf import settings
 from django.db import transaction
-from rest_framework import status
+from rest_framework import status, viewsets, mixins
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from borrowing.serializers import BorrowingReturnSerializer
 from payment.models import Payment
-from payment.serializers import EmptySerializer
+from payment.serializers import EmptySerializer, PaymentSerializer
 
 
 class StripeSuccessAPI(APIView):
@@ -87,3 +87,34 @@ class StripeCancelAPI(APIView):
 
     def get(self, request):
         return Response({"message": "Payment was cancelled. You can try again."})
+
+
+class PaymentListView(APIView):
+    """
+    Allows to see all payments fow administrators
+    and list of their own payments for users.
+    """
+    def get(self, request):
+        if self.request.user.is_staff:
+            payments = Payment.objects.all()
+        else:
+            payments = Payment.objects.filter(borrowing__user__id=self.request.user.id)
+        serializer = PaymentSerializer(payments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PaymentDetailView(APIView):
+    """
+    Allows to see details about the payment.
+    """
+    def get(self, request, pk):
+        try:
+            payment = Payment.objects.get(pk=pk)
+        except Payment.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        if not request.user.is_staff and payment.borrowing.user != request.user:
+            return Response({"detail": "You do not have permission to view this payment."},
+                            status=status.HTTP_403_FORBIDDEN)
+        serializer = PaymentSerializer(payment)
+        return Response(serializer.data, status.HTTP_200_OK)
