db0ad6c72216f401cdfcbf46728093f2ac6e6855
10kkyvl <ztkkre@gmail.com>
2025-03-03 23:13:53 +0200

    feat: added retry if request didnt success

--- src/notification/utils.py
+++ src/notification/utils.py
@@ -9,13 +9,23 @@ load_dotenv()
 API_TOKEN = os.getenv("API_KEY")
 API_URL = f"https://api.telegram.org/bot{API_TOKEN}/"
 
-@shared_task
-def send_message(chat_id: int, text: str):
+@shared_task(bind=True, max_retries=3, default_retry_delay=60)
+def send_message(self, chat_id: int, text: str):
     data = {
         "chat_id": chat_id,
         "text": text
     }
-    requests.post(API_URL + "sendMessage", params=data)
+
+    try:
+        response = requests.post(API_URL + "sendMessage", params=data)
+
+        if response.status_code != 200:
+            raise self.retry(exc=f"Received status code {response.status_code} instead of 200.")
+
+        return response.json()
+
+    except Exception as exc:
+        raise self.retry(exc=exc)
 
 
 def get_bot_username() -> str:
