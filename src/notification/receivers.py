33f064d3f29028cea8e3e9d7ff92b9cb74086c3f
10kkyvl <ztkkre@gmail.com>
2025-03-03 21:46:13 +0200

    remade to to_admin_chat so u dont need to django.setup()

--- src/notification/receivers.py
+++ src/notification/receivers.py
@@ -1,9 +1,16 @@
+import os
+
 from django.dispatch import receiver
+from dotenv import load_dotenv
 
 from notification.signals import notification
 from notification.utils import send_message
 
 
+load_dotenv()
+
 @receiver(notification)
-def notify(sender, chat_id: int, message: str, **kwargs):
+def notify(sender, message: str, chat_id: int = None, **kwargs):
+    if kwargs.get("to_admin_chat", None):
+        chat_id = os.environ.get("CHAT_ID")
     send_message(chat_id, message)
