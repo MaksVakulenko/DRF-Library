3f13a64d6f4af738c9c32e71b4b7f31b1b379c00
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-03-04 14:49:10 +0200

    Fix migrations

--- src/borrowing/serializers.py
+++ src/borrowing/serializers.py
@@ -86,9 +86,7 @@ class BorrowingListSerializer(serializers.ModelSerializer):
         today = datetime.date.today()
         if self.get_is_active(obj) and today > obj.expected_return_date:
             days_expired = (today - obj.expected_return_date).days
-            return int(
-                (obj.book.daily_fee * days_expired) * 100
-            ) * FINE_MULTIPLIER
+            return int((obj.book.daily_fee * days_expired) * FINE_MULTIPLIER)
         return None
 
     def to_representation(self, instance):
