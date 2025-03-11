9ccdef9f20f29a6464f5cdfe8b62c5d80026a47c
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-03-11 14:21:45 +0200

    Remove ukrainina comments and move email message to the separate file

--- src/borrowing/tasks.py
+++ src/borrowing/tasks.py
@@ -6,6 +6,7 @@ from borrowing.models import Borrowing
 from library_service.settings import FRONTEND_URL
 from notification.signals import notification
 from user.tasks import send_verification_email_task
+from library_service.messages import get_message_overdue_borrowings, get_email_overdue_message
 
 
 @shared_task
@@ -23,32 +24,13 @@ def check_overdue_borrowings():
         )
 
     for borrowing in overdue_borrowings:
-        admin_message = (
-            f"📚 Overdue Borrowing Alert!\n\n"
-            f"👤 User: {borrowing.user}\n"
-            f"🔹 Book: {borrowing.book}\n"
-            f"📅 Borrowed on: {borrowing.borrow_date.strftime('%B %d, %Y')}\n"
-            f"📅 Expected return: {borrowing.expected_return_date.strftime('%B %d, %Y')}"
-        )
+        admin_message = get_message_overdue_borrowings(borrowing)
         notification.send(sender=None, message=admin_message, to_admin_chat=True)
         today = datetime.now().date()
         days_expired = (today - borrowing.expected_return_date).days
         fine = int((borrowing.book.daily_fee * days_expired) * 100) * 2
 
-        email_message = (
-            f"Hello, {borrowing.user.first_name}!\n\n"
-            f"We noticed that the book \"{borrowing.book.title}\" you borrowed on "
-            f"{borrowing.borrow_date.strftime('%B %d, %Y')} was due for return on "
-            f"{borrowing.expected_return_date.strftime('%B %d, %Y')}.\n\n"
-            f"As of today, it is overdue by {days_expired} day(s). Please return the book as soon as possible to avoid further penalties.\n\n"
-            f"A fine of ${fine / 100:.2f} has been applied to your account.\n\n"
-            f"Use provided link to pay it:"
-            f"{FRONTEND_URL}/api/borrowings/{borrowing.id}/return/"
-            "If you have any questions, feel free to reach out.\n\n"
-            "Thank you for your cooperation!\n\n"
-            "Best regards,\n"
-            "Your Library Team 📚"
-        )
+        email_message = get_email_overdue_message(borrowing=borrowing, days_expired=days_expired, fine=fine, frontend_url=FRONTEND_URL)
 
         send_verification_email_task.delay(
             subject="Overdue borrowing",
--- src/library_service/messages.py
+++ src/library_service/messages.py
@@ -27,3 +27,28 @@ def get_message_payment_successful(payment):
         f"💸 Amount: {payment.amount_of_money // 100} USD\n"
         f"📅 Date: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}"
     )
+
+def get_message_overdue_borrowings(borrowing):
+    return (
+        f"📚 Overdue Borrowing Alert!\n\n"
+        f"👤 User: {borrowing.user}\n"
+        f"🔹 Book: {borrowing.book}\n"
+        f"📅 Borrowed on: {borrowing.borrow_date.strftime('%B %d, %Y')}\n"
+        f"📅 Expected return: {borrowing.expected_return_date.strftime('%B %d, %Y')}"
+    )
+
+def get_email_overdue_message(borrowing, days_expired, fine, frontend_url):
+    return (
+        f"Hello, {borrowing.user.first_name}!\n\n"
+        f"We noticed that the book \"{borrowing.book.title}\" you borrowed on "
+        f"{borrowing.borrow_date.strftime('%B %d, %Y')} was due for return on "
+        f"{borrowing.expected_return_date.strftime('%B %d, %Y')}.\n\n"
+        f"As of today, it is overdue by {days_expired} day(s). Please return the book as soon as possible to avoid further penalties.\n\n"
+        f"A fine of ${fine / 100:.2f} has been applied to your account.\n\n"
+        f"Use provided link to pay it:"
+        f"{frontend_url}/api/borrowings/{borrowing.id}/return/"
+        "If you have any questions, feel free to reach out.\n\n"
+        "Thank you for your cooperation!\n\n"
+        "Best regards,\n"
+        "Your Library Team 📚"
+    )
